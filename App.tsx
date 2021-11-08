936f8bf73d0182a4c06828434a1e63e32558afea
grzes5003 <grzes.kulinski@gmail.com>
2021-11-08 21:57:05 +0100

    Multiple changes to project, added redux

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from './navigation';
+
+import {store} from "./_helpers/store.helper";
+import {Provider} from 'react-redux';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <Provider store={store}>
+                    <Navigation colorScheme={colorScheme}/>
+                    <StatusBar/>
+                </Provider>
+            </SafeAreaProvider>
+        );
+    }
+}
